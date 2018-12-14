6663eba0e79b56768a7a1ecaa8892165e8daceb2
karenhanson <karenhanson77@gmail.com>
2018-12-14 12:59:07 -0500

    add missing import

--- integration/src/test/java/info/rmapproject/integration/SmokeTestIT.java
+++ integration/src/test/java/info/rmapproject/integration/SmokeTestIT.java
@@ -12,7 +12,6 @@ import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Base64;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.ResponseBody;
